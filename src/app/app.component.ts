4fb606aa54c43b72e7d462ced7f1ec12ea7ad66a
Karan Kumar <officialkaran39@gmail.com>
2025-03-06 12:22:41 +0530

    [Karan]Add. Ensuring the userName is the valid input consisting of initial caps and min 3 letter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  
-  
   imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -15,6 +13,7 @@ export class AppComponent {
   imgUrl = "D:\AngularSetup\src\assets\logo.jpeg";
   url = "https://www.bridgelabz.com";
   userName: string= "";
+  nameError: string = "";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz";
@@ -22,5 +21,14 @@ export class AppComponent {
   onClick($event:MouseEvent ){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+  onInput($event: Event){
+    console.log("Change Event Occured!",($event.target as HTMLInputElement).value);
+    const nameRagex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRagex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
